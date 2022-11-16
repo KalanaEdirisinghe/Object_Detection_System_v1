f19b2fb4eabf13ebdfda804e431833f681271fd8
kalana geesara <69765394+KalanaEdirisinghe@users.noreply.github.com>
2022-11-16 10:22:41 +0530

    Add files via upload

---
+++ fromObjDetect.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ObjectDetection
+{
+    partial class fromObjDetect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnScreenShot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(208, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Object Detection";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(77, 180);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(198, 99);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnScreenShot
+            // 
+            this.btnScreenShot.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScreenShot.Location = new System.Drawing.Point(375, 180);
+            this.btnScreenShot.Name = "btnScreenShot";
+            this.btnScreenShot.Size = new System.Drawing.Size(198, 99);
+            this.btnScreenShot.TabIndex = 1;
+            this.btnScreenShot.Text = "SCREENSHOT FOLDER";
+            this.btnScreenShot.UseVisualStyleBackColor = true;
+            this.btnScreenShot.Click += new System.EventHandler(this.btnScreenShot_Click);
+            // 
+            // fromObjDetect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 381);
+            this.Controls.Add(this.btnScreenShot);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Name = "fromObjDetect";
+            this.Text = "Object Detection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnScreenShot;
+    }
+}
+
---
+++ fromObjDetect.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ObjectDetection
+{
+    public partial class fromObjDetect : Form
+    {
+        public fromObjDetect()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Creating new process info
+            ProcessStartInfo psi = new ProcessStartInfo();
+            string virtualEnvironment = getPath() + "venv\\Scripts\\python.exe";
+            psi.FileName = virtualEnvironment;
+
+            // Scripts and args
+            var script = getPath() + "Face-Mask-Detection-system\\detect_mask_video.py";
+            psi.Arguments = $"\"{script}\"";
+
+            // Process config
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = false;
+            psi.RedirectStandardError = false;
+
+            //var process = Process.Start(psi);
+            //string errors = "Errors\n" + process.StandardError.ReadToEnd();
+            //string output = "Output\n" + process.StandardOutput.ReadToEnd();
+
+            //MessageBox.Show(errors, "Errors");
+            //MessageBox.Show(output, "Outputs");
+
+            ThreadStart newThreadInf = new ThreadStart(() => Process.Start(psi));
+            Thread newThread = new Thread(newThreadInf);
+            newThread.Start();
+        }
+
+        private void btnScreenShot_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", getPath() + "Without_Mask_Images");
+        }
+       
+        private string getPath()
+        {
+            var path = Directory.GetCurrentDirectory();
+            string[] mainPath = path.Split('\\');
+            int n = mainPath.Length;
+            string ans = "";
+            for(int i = 0; i < n - 4; i++)
+            {
+                ans += mainPath[i] + "\\";
+            }
+            return ans;
+        }
+    }
+}
